b77edc3c099cbf4b0df87bfc16a8270b01b41ed7
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-08-12 08:29:49 +0200

    added a comment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,9 @@ import {ContextStyling} from "../App"
 const Button = () => {
   const value = React.useContext(ContextStyling)
   console.log(value)
+
+  // const { light, toggleTheme } = React.useContext(ThemeContext) you can also do object destructuring for the value so you skip the . going into the object
+
   return (
     <button className={`${value.light}-theme`} onClick={value.toggleTheme}>
         Switch Theme
